72ddfee99be096c0a0e57efd83b5375cd1eac586
Doxxus <lockycampbell@cmail.carleton.ca>
2018-12-15 17:58:11 -0500

    Model created

--- main.py
+++ main.py
@@ -9,43 +9,46 @@ import data_prep as data
 
 print("--- Dependancies Loaded ---")
 
-data.process_data()
 m_data, idx_q, idx_a = data.load_data()
+(trX, trY), (teX, teY), (vaX, vaY) = data.split_dataset(idx_q, idx_a)
 
 print("--- Data Loaded ---")
 
 #Hyperparameters
-epochs = 100
-batch_size = 128
+epochs = 10000
+batch_size = 64
 
-rnn_size = 512
-layers = 2
-encoding_size = 512
-decoding_size = 512
+x_len = trX.shape[-1]
+y_len = trY.shape[-1]
 
-alpha = 0.005
-alpha_decay = 0.9
-alpha_min = 0.0001
+layers = 3
+xvocab_size = len(metadata['idx2w'])
+yvocab_size = xvocab_size
+emb_dim = 1024
+
+alpha = 0.0001
 p_keep = 0.75
 
-#Begin working on model here
+#Input placeholders
+enc_ip = [tf.placeholder(shape=[None,], dtype=tf.int64, name='ei_{}'.format(t)) for t in range(x_len)]
+labels = [tf.placeholder(shape=[None,], dtype=tf.int64, name='ei_{}'.format(t)) for t in range(y_len)]
+dec_ip = [tf.zeros_like(enc_ip[0], dtype=tf.int64, name='GO')] + labels[:-1]
 
-#Creates the Encoder model
-def encoder(input, n, l, p_k, s_len):
-    lstm = tf.contrib.rnn.LSTMCell(n)
-    dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob = p_k)
-    cell = tf.contrib.rnn.MultiRNNCell([dropout] * l)
-    _, state = tf.nn.bidirectional_dynamic_rnn(cell_fw = cell,
-                                               cell_bw = cell,
-                                               sequence_length = s_len,
-                                               inputs = input, 
-                                               dtype=tf.float32)
-    return state
+#Keep Probability placeholder
+keep_prob = tf.placeholder(tf.float32)
+
+#Creating the Encoder
+enc_cell = tf.nn.rnn_cell.LSTMCell(emb_dim)
+dropout = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob = keep_prob)
+enc_state = tf.nn.rnn_cell.MultiRNNCell([dropout]*layers)
+ 
+#Building the Decoder and the Sequence to Sequence model 
+with tf.variable_scope('decoder') as scope:
+	decode_out, decode_states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim)
+	scope.reuse_variables()
+	decode_out_test, decode_states_test = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim, feed_previous=True)
 	
-#Decodes the training data
-def decoder_train(enc_state, d_cell, d_input, s_len, d_scope, o_fn, p_k, b_size):
-	attention_states = tf.zeros([b_size, 1, d_cell.output_size])
-	
-
-
+	loss_weights = [tf.ones_like(label, dtype=tf.float32) for label in labels]
+	cost = tf.contrib.legacy_seq2seq.sequence_loss(decode_out, labels, loss_weights, yvocab_size)
+	train_op = tf.train.AdamOptimizer(alpha).minimize(cost)
 	
