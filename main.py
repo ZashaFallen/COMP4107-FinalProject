873218d8bc56f5382545a2e794e4e6ba9000f23a
Doxxus <lockycampbell@cmail.carleton.ca>
2018-12-15 20:03:27 -0500

    DUCC ME SUCC

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ with tf.variable_scope('decoder') as scope:
     setattr(tf.contrib.rnn.GRUCell, '__deepcopy__', lambda self, _: self)
     setattr(tf.contrib.rnn.BasicLSTMCell, '__deepcopy__', lambda self, _: self)
     setattr(tf.contrib.rnn.MultiRNNCell, '__deepcopy__', lambda self, _: self)
-	decode_out, decode_states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim)
-	scope.reuse_variables()
-	decode_out_test, decode_states_test = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim, feed_previous=True)
+    decode_out, decode_states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim)
+    scope.reuse_variables()
+    decode_out_test, decode_states_test = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(enc_ip, dec_ip, enc_state, xvocab_size, yvocab_size, emb_dim, feed_previous=True)
 
 
     loss_weights = [tf.ones_like(label, dtype=tf.float32) for label in labels]
